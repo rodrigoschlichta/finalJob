68aa9a3d5dd818440dd2a09ca8c761256d6216a9
rodrigoschlichta <rodrigo.schlichta@gmail.com>
2021-01-24 09:24:24 -0300

    Project Created

---
+++ backend/src/main/java/com/bootcamp/finaljob/FinaljobApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.finaljob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaljobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaljobApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bootcamp/finaljob/FinaljobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.finaljob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaljobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
